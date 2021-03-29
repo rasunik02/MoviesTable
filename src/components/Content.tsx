fe2f45faa6ea5e6b5831e8dc71ca536f79c35214
Fernando Matos <fernandomatos@localhost.localdomain>
2021-03-29 08:13:28 -0300

    Desafio Feito

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,43 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+
+interface Genre {
+  id: number;
+  title: string;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+}
+
+interface Movie {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  selectedGenre: Genre;
+  movies: Movie[]
+}
+
+
+export function Content({movies, selectedGenre}: ContentProps) {
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+    
+  )
 }
